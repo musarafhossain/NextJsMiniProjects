5445edeb51664db7086f251b1ec6975b10afe997
Musaraf Hossain <myselfmusarafhossain786@gmail.com>
2024-07-02 00:38:20 +0530

    highlight selected item

--- slider/app/help/page.jsx
+++ slider/app/help/page.jsx
@@ -1,20 +1,30 @@
 "use client";
 import Navbar from "@/components/navbar/Navbar";
 import Slider from "@/components/slider/Slider";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Help() {
-  const [isToggle, setIsToggle] = useState(false);
+  const [isToggle, setIsToggle] = useState(true);
 
   const toggleHandle = () => {
     setIsToggle(!isToggle);
   };
 
+  // Function to check if the device is mobile
+  const isMobileDevice = () => window.matchMedia("(max-width: 768px)").matches;
+
+  useEffect(() => {
+    // Set isToggle to false if the device is mobile
+    if (isMobileDevice()) {
+      setIsToggle(false);
+    }
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar toggleHandle={toggleHandle} />
       <div className="flex">
-        <Slider isToggle={isToggle} />
+        <Slider isToggle={isToggle} page='help'/>
         <div className="flex w-full items-center justify-center p-4 slider-height overflow-x-hidden overflow-auto">
           <h1 className="text-4xl font-bold text-yellow-400">This is upcoming help page...</h1>
         </div>
