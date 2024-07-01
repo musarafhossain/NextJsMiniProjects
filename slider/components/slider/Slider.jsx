1b61c050a865e7c61026984b9066b98d84703888
Musaraf Hossain <myselfmusarafhossain786@gmail.com>
2024-07-01 12:23:42 +0530

    update slider items smoothnes

--- slider/components/slider/Slider.jsx
+++ slider/components/slider/Slider.jsx
@@ -8,8 +8,8 @@ import SliderItems from './SliderItems';
 const Slider = ({isToggle}) => {
   return (
     <div className={`duration-300 border-r absolute md:static border-gray-300 slider-height bg-gray-100 ${isToggle?' w-72 ':' w-0 '}`}>
-        <div className={`${isToggle?' block ':' hidden '} duration-200`}>
-            <div className='text-nowrap flex flex-col py-5 px-2 gap-1'>
+        <div className={`scale-box ${isToggle?' flex zoom-scale-box ':' hidden '} duration-200`}>
+            <div className='text-nowrap w-full flex flex-col py-5 px-2 gap-1'>
                 <SliderItems Icon={FaHome} link='/' title='Home'/>
                 <SliderItems Icon={MdDashboard} link='/dashboard' title='Dashboard'/>
                 <SliderItems Icon={MdOutlineHelpOutline} link='/help' title='Help'/>
